9bdd44e31199686da39e84783b81049c810794c1
JHW <jhw010406@gmail.com>
2024-09-02 12:05:12 +0900

    testinupdate

--- app/src/main/java/com/example/tradingapp/MainActivity.kt
+++ app/src/main/java/com/example/tradingapp/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TradingAppTheme {
+                var test : Int
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "okTHX",
